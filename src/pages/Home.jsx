8337cb80a62553beede76ad6100c32ac054fe204
Roman <rosipenko@yandex.ru>
2022-10-08 22:42:37 +0500

    start

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Calculator from '../components/Calculator';
+
+const Home = () => {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+};
+
+export default Home;
